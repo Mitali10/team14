6bf2b4e0be7ecc94123185e657c058ae7d4cd030
m-skaan <93020882+m-skaan@users.noreply.github.com>
2022-11-13 01:03:03 -0500

    Undersampling
    used RandomUnderSampler in imblearn to make an undersampling function.

--- src/CreditCardData.py
+++ src/CreditCardData.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler, NearMiss
 
 import sys
 # def reduce_dataset_size(from_filepath='../data/creditcard.csv', to_filepath='../data/creditcard_sampled2.csv', proportion=0.02):
@@ -21,10 +22,13 @@ class CreditCardData:
     '''
     Class for Credit Card Data. Can (and should) be used by all classes that handle this data. 
     '''
+
     def __init__(self, filepath="../data/creditcard.csv", split=False, test_prop=0.2):
-        csv_data_og = np.loadtxt(open(filepath, "rb"), dtype=str, delimiter=",", skiprows=1, ndmin=2)
+        csv_data_og = np.loadtxt(open(filepath, "rb"),
+                                 dtype=str, delimiter=",", skiprows=1, ndmin=2)
         self.data = np.array(csv_data_og[:, :-1]).astype(float)
-        self.labels = np.array([row[-1].replace("\"", "") for row in csv_data_og]).astype(int)
+        self.labels = np.array([row[-1].replace("\"", "")
+                               for row in csv_data_og]).astype(int)
 
         self.N = self.data.shape[0]
         self.D = self.data.shape[1]
@@ -32,12 +36,15 @@ class CreditCardData:
 
         self.data_train, self.data_test, self.labels_train, self.labels_test = None, None, None, None
         if split:
-            self.data_train, self.data_test, self.labels_train, self.labels_test = train_test_split(self.data, self.labels, test_size=test_prop)
-    def split_data(self, test_size = 0.2, iterations = 1): 
+            self.data_train, self.data_test, self.labels_train, self.labels_test = train_test_split(
+                self.data, self.labels, test_size=test_prop)
+
+    def split_data(self, test_size=0.2, iterations=1):
         X = self.data
         y = self.labels
         X_train, X_test, y_train, y_test = ([] for i in range(4))
-        sss = StratifiedShuffleSplit(n_splits=iterations, test_size=test_size, random_state=0)
+        sss = StratifiedShuffleSplit(
+            n_splits=iterations, test_size=test_size, random_state=0)
         sss.get_n_splits(X, y)
         for train_index, test_index in sss.split(X, y):
             X_train.append(X[train_index])
@@ -45,15 +52,22 @@ class CreditCardData:
             y_train.append(y[train_index])
             y_test.append(y[test_index])
         return np.asarray(X_train), np.asarray(X_test), np.asarray(y_train), np.asarray(y_test)
-    def oversample(self, X_train, y_train): 
-        X_resampled , y_resampled = ([] for i in range(2))
+
+    def oversample(self, X_train, y_train):
+        X_resampled, y_resampled = ([] for i in range(2))
         for i in range(len(X_train)):
-            smote = SMOTE(sampling_strategy = 0.1) # desired ratio minority/majority = 0.1
+            # desired ratio minority/majority = 0.1
+            smote = SMOTE(sampling_strategy=0.1)
             X, y = smote.fit_resample(X_train[i], y_train[i])
             X_resampled.append(X)
             y_resampled.append(y)
         return np.asarray(X_resampled), np.asarray(y_resampled)
 
-        
-
-
+    def undersample(self, X_train, y_train):
+        X_resampled, y_resampled = ([] for i in range(2))
+        for i in range(len(X_train)):
+            under = RandomUnderSampler(sampling_strategy = 0.5)
+            X, y = under.fit_resample(X_train[i], y_train[i])
+            X_resampled.append(X)
+            y_resampled.append(y)
+        return  np.asarray(X_resampled), np.asarray(y_resampled)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,10 +2,23 @@ import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
 from CreditCardData import CreditCardData
 
+
+## doing oversampling by itself
 obj = CreditCardData()
 X_train, X_test, y_train, y_test = obj.split_data()
-print("Y COUNT",np.unique(y_train[0], return_counts=True) )
+print("Y COUNT", np.unique(y_train[0], return_counts=True))
 X_resampled, y_resampled = obj.oversample(X_train, y_train)
 print("Y RESAMPLED COUNT", np.unique(y_resampled[0], return_counts=True))
 
-    
+
+## doing undersampling by itself
+new_obj = CreditCardData()
+X_train1, X_test1, y_train1, y_test1 = new_obj.split_data()
+##print(new_obj.tomek_undersample(X_train1, y_train1)) --> can't use tomek bc too many features in data
+print("Y COUNT", np.unique(y_train1[0], return_counts = True))
+X_resampled1, y_resampled1 = new_obj.oversample(X_train1, y_train1)
+print("Y RESAMPLED COUNT", np.unique(y_resampled1[0], return_counts=True))
+
+
+## can also combine oversampling and undersampling
+## idea from machine learning mastery blog: https://machinelearningmastery.com/random-oversampling-and-undersampling-for-imbalanced-classification/
