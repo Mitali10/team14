3017d560a206063a854e2fc057458c333eccebe4
Gandhe, Mitali Parag <mgandhe3@gatech.edu>
2022-12-05 22:55:23 -0500

    random forst performance vs. max tree depth

--- src/random_forest.py
+++ src/random_forest.py
@@ -3,9 +3,11 @@ import numpy as np
 from CreditCardData import CreditCardData
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import mixture, metrics
-from sklearn.cluster import KMeans, DBSCAN
 from imblearn.over_sampling import SMOTE
 
+from sklearn.metrics import roc_auc_score
+
+import time
 
 ccd = CreditCardData(split=True)
 ccd.split_data()
@@ -57,57 +59,95 @@ X_smote, y_smote = smote.fit_resample(ccd.data_train, ccd.labels_train)
 # plt.scatter(ccd.data_test[:, 0], ccd.data_test[:, 1], c=labels_pred, s=40, cmap='viridis')
 # plt.show()
 
+info = {
+  "Recall": [],
+  "Balanced Accuracy": [],
+  "Precision": [],
+  "AUC-ROC": [],
+  "CM": [],
+  "Accuracy": [],
+}
 
-forest = RandomForestClassifier(max_depth=20, random_state=0)
-forest.fit(X_smote, y_smote)
-labels_pred_test = forest.predict(ccd.data_test)
-score = forest.score(ccd.data_test, ccd.labels_test)
-print("SCORE FOR FOREST", score)
+maxdepths = range(10, 20, 2)
+for i in maxdepths:
+  print (f"\n\n------- i = {i} --------------")
+  start = time.time()
+  forest = RandomForestClassifier(max_depth=i, random_state=0, verbose=True)
+  forest.fit(X_smote, y_smote)
+  labels_pred_test = forest.predict(ccd.data_test)
+  score = forest.score(ccd.data_test, ccd.labels_test)
+  print("SCORE FOR FOREST", score)
+
+  info["Accuracy"].append(score)
+
+  confusion_matrix = metrics.confusion_matrix(ccd.labels_test, labels_pred_test, normalize = 'true')
+  print(confusion_matrix); 
+
+  recall = metrics.recall_score(ccd.labels_test, labels_pred_test, average='macro')
+  print("Recall:", recall)
+  info["Recall"].append(recall)
 
 
-confusion_matrix = metrics.confusion_matrix(ccd.labels_test, labels_pred_test, normalize = 'true')
-print(confusion_matrix); 
-
-recall = metrics.recall_score(ccd.labels_test, labels_pred_test, average='macro')
-print("Recall:", recall)
+  balanced_accuracy = metrics.balanced_accuracy_score(ccd.labels_test, labels_pred_test)
+  print("balanced_accuracy:", balanced_accuracy)
+  info["Balanced Accuracy"].append(balanced_accuracy)
 
 
-balanced_accuracy = metrics.balanced_accuracy_score(ccd.labels_test, labels_pred_test)
-print("balanced_accuracy:", balanced_accuracy)
-
-precision_score = metrics.precision_score(ccd.labels_test, labels_pred_test)
-print("precision_score:", precision_score)
-
-report = metrics.classification_report(ccd.labels_test, labels_pred_test)
-print(report)
+  precision_score = metrics.precision_score(ccd.labels_test, labels_pred_test)
+  print("precision_score:", precision_score)
+  info["Precision"].append(precision_score)
 
 
-############################## POST-PROCESSING: CONFUSION MATRIX ##############################
+  report = metrics.classification_report(ccd.labels_test, labels_pred_test)
+  print(report)
 
-# calc confusion matix
-cm = metrics.confusion_matrix(ccd.labels_test, labels_pred_test)
-print("Confusion Matrix\n", cm)
 
-# show confusion matrix
-plt.figure(figsize=(9,9))
-plt.imshow(cm, cmap='Blues')
-plt.title('Confusion matrix', size=15)
-plt.colorbar()
+  ############################## POST-PROCESSING: CONFUSION MATRIX ##############################
 
-plt.xticks([0, 1], ["0", "1"], rotation=45, size=2)
-plt.yticks([0, 1], ["0", "1"], size = 2)
-plt.tight_layout()
-plt.ylabel('Actual label', size=15)
-plt.xlabel('Predicted label', size = 15)
-width, height = cm.shape
-for x in range(width):
- for y in range(height):
-  plt.annotate(str(cm[x][y]), xy=(y, x), 
-    horizontalalignment='center',
-    verticalalignment='center')
+  aucroc = roc_auc_score(ccd.labels_test, labels_pred_test)
+  print("AUCROC:", aucroc)
 
+  info["AUC-ROC"].append(aucroc)
+
+
+
+  # calc confusion matix
+  cm = metrics.confusion_matrix(ccd.labels_test, labels_pred_test)
+  print("Confusion Matrix\n", cm)
+  info["CM"].append(cm)
+  end = time.time()
+  print("TIME", (end - start)/60)
+
+# # show confusion matrix
+# plt.figure(figsize=(9,9))
+# plt.imshow(cm, cmap='Blues')
+# plt.title('Confusion matrix', size=15)
+# plt.colorbar()
+
+# plt.xticks([0, 1], ["0", "1"], rotation=45, size=2)
+# plt.yticks([0, 1], ["0", "1"], size = 2)
+# plt.tight_layout()
+# plt.ylabel('Actual label', size=15)
+# plt.xlabel('Predicted label', size = 15)
+# width, height = cm.shape
+# for x in range(width):
+#  for y in range(height):
+#   plt.annotate(str(cm[x][y]), xy=(y, x), 
+#     horizontalalignment='center',
+#     verticalalignment='center')
+
+# plt.show()
+
+print(info)
+
+# plot lines
+plt.plot(maxdepths, info["Recall"], label = "Recall")
+# plt.plot(maxdepths, info["AUC-ROC"], label = "AUC-ROC")
+plt.plot(maxdepths, info["Precision"], label = "Precision")
+plt.plot(maxdepths, info["Accuracy"], label = "Accuracy")
+plt.title('Performance vs Max Tree Depth')
+plt.xlabel('Max Tree Depth')
+plt.ylabel('Performance')
+plt.legend()
 plt.show()
-
-
-
 
